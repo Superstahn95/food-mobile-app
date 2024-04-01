ee0c3704b231c0c321bfae5794c1cdd5d1085043
superstahn <superstahn@gmail.com>
2024-04-01 13:23:05 +0100

    Initial commit of the app with the welcome screen done. The UI for each screen is still open to changes

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  useFonts,
+  Roboto_100Thin,
+  Roboto_500Medium,
+  Roboto_700Bold,
+  Roboto_400Regular,
+} from "@expo-google-fonts/roboto";
+import {
+  Montserrat_400Regular,
+  Montserrat_700Bold,
+  Montserrat_300Light,
+} from "@expo-google-fonts/montserrat";
+import { StyleSheet, Text, View } from "react-native";
+import WelcomeScreen from "./screens/WelcomeScreen";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Roboto_100Thin,
+    Roboto_500Medium,
+    Roboto_700Bold,
+    Roboto_400Regular,
+    Montserrat_400Regular,
+    Montserrat_700Bold,
+    Montserrat_300Light,
+  });
+  if (!fontsLoaded) {
+    return <View></View>;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WelcomeScreen />
+    // <View style={styles.container}>
+    //   <Text style={styles.text}>Welcome to react native</Text>
+    //   <StatusBar style="auto" />
+    // </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "white",
   },
 });
---
+++ screens/WelcomeScreen.js
@@ -0,0 +1,151 @@
+import {
+  Text,
+  View,
+  StyleSheet,
+  ImageBackground,
+  Dimensions,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+const screenHeight = Dimensions.get("window").height;
+const screenWidth = Dimensions.get("window").width;
+
+function WelcomeScreen() {
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        style={styles.background}
+        source={require("../assets/images/pizza.jpg")}
+      >
+        <View style={styles.darkCover} />
+
+        <View style={styles.contentWrapper}>
+          <View style={styles.logoContainer}>
+            {/* "#fdd835" */}
+            {/* #1E293B */}
+            {/* "#f57f17"  */}
+            <Image
+              style={styles.logo}
+              source={require("../assets/images/Rooku.jpg")}
+            />
+            {/* <Ionicons name="fast-food" size={50} color="#f57f17" /> */}
+          </View>
+          <Text style={styles.welcomeText}>FIND FOOD YOU LOVE </Text>
+          <Text style={styles.subtitle}>
+            Discover the best food from over 200 varieties and delivery to your
+            doorstep
+          </Text>
+          <View style={styles.controls}>
+            <TouchableOpacity style={styles.logIn}>
+              <Text style={styles.loginText}>Log in</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.register}>
+              <Text style={styles.registerText}>Get started</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ImageBackground>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    position: "relative",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  background: {
+    height: screenHeight,
+    width: screenWidth,
+  },
+  darkCover: {
+    position: "absolute",
+    backgroundColor: "black",
+    opacity: 0.5,
+    position: "absolute",
+    alignItems: "center",
+    top: 0,
+    left: 0,
+    width: screenWidth,
+    height: screenHeight,
+  },
+  contentWrapper: {
+    // backgroundColor: "white",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 10,
+    backgroundColor: "white",
+  },
+  welcomeText: {
+    color: "#1E293B",
+    fontSize: 25,
+    fontWeight: "bold",
+    fontFamily: "Montserrat_700Bold",
+    marginTop: 20,
+  },
+  subtitle: {
+    fontFamily: "Montserrat_400Regular",
+    marginTop: "5%",
+    color: "#e65100",
+    fontWeight: "bold",
+    fontSize: 20,
+    textAlign: "center",
+  },
+  logoContainer: {
+    // marginTop: "20%",
+  },
+  logo: {
+    height: 200,
+    width: 200,
+  },
+  welcomeBox: {
+    // backgroundColor: "white",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    // padding: 20,
+    fontSize: 30,
+  },
+  controls: {
+    width: "100%",
+    marginTop: "40%",
+    // marginBottom: "50%",
+  },
+  //   #e65100
+  // #f57f17"
+  logIn: {
+    backgroundColor: "#e65100",
+    paddingHorizontal: 7,
+    paddingVertical: 10,
+    color: "white",
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 4,
+    borderRadius: 2,
+  },
+  loginText: {
+    color: "white",
+    fontFamily: "Montserrat_700Bold",
+  },
+  register: {
+    backgroundColor: "#1E293B",
+    // borderWidth: 2,
+    // borderColor: "red",
+    paddingHorizontal: 7,
+    paddingVertical: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 5,
+  },
+  registerText: {
+    color: "white",
+    fontFamily: "Montserrat_700Bold",
+  },
+});
+
+export default WelcomeScreen;
